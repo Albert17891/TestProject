4f02e20af96d2028de44d9c4bdbd49dc4597ddd6
Albert <abogevorgian@gmail.com>
2023-06-05 12:13:39 +0400

    add Api Models

---
+++ MovieApp/MovieApp.Web/Models/Request/MovieRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApp.Web.Models.Request;
+
+public class MovieRequest
+{
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string Description { get; set; }
+    public DateTime CreatedDate { get; set; }
+}
