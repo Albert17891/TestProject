67258b230f1b4d25661075d9cca4848bc8e5b50f
Albert <abogevorgian@gmail.com>
2023-06-05 11:50:33 +0400

    add configuration

---
+++ MovieApp/MovieApp.Infastructure/Configuration/MovieConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MovieApp.Core.Entities;
+
+namespace MovieApp.Infastructure.Configuration;
+
+public class MovieConfiguration : IEntityTypeConfiguration<Movie>
+{
+    public void Configure(EntityTypeBuilder<Movie> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x=>x.Name).IsRequired();
+        builder.Property(x=>x.Description).IsRequired();
+        builder.Property(x => x.CreatedDate).IsRequired();
+
+        builder.HasData(new Movie
+        {
+            Id = 1,
+            Name = "Test",
+            Description = "Some Test",
+            CreatedDate = DateTime.Now,
+        });
+    }
+}
