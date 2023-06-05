276ff58a12fece91a9b2340bfe9976ddccf7920f
Albert <abogevorgian@gmail.com>
2023-06-05 12:10:17 +0400

    add ServiceCollectionExtensions

--- MovieApp/MovieApp.Core/Entities/Movie.cs
+++ MovieApp/MovieApp.Core/Entities/Movie.cs
@@ -2,8 +2,8 @@
 
 public class Movie
 {
-    public int Id { get; set; } 
-    public string Name { get; set; }    
-    public string Description { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;   
+    public string Description { get; set; }= string.Empty;
     public DateTime CreatedDate { get; set;}
 }
---
+++ MovieApp/MovieApp.Infastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MovieApp.Core.Interfaces;
+using MovieApp.Infastructure.Data;
+
+namespace MovieApp.Infastructure;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfastructureServices(this IServiceCollection services,IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IMovieRepository, MovieRepository>();    
+
+        return services;
+    }
+}
