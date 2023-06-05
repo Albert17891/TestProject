39696dcd57c1082c6b3c67d9757684825d971d33
Albert <abogevorgian@gmail.com>
2023-06-05 12:44:45 +0400

    add new models

---
+++ MovieApp/MovieApp.Web/Models/Request/NewFolder/Validation/MovieUptadeRequestValidation.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace MovieApp.Web.Models.Request.NewFolder.Validation;
+
+public class MovieUptadeRequestValidation:AbstractValidator<MovieUpdateRequest>
+{
+    public MovieUptadeRequestValidation()
+    {
+        RuleFor(x => x.Id)
+            .NotNull()
+            .WithMessage("id is required");
+
+        RuleFor(x => x.Name)
+            .NotNull()
+            .WithMessage("Name cannot be null");
+
+        RuleFor(x => x.Description)
+            .NotNull()
+            .WithMessage("Description cannot be null");
+    }
+}
