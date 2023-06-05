6a0096fe8f08ef698b301ae6aa51cb78d5a92766
Albert <abogevorgian@gmail.com>
2023-06-05 11:59:23 +0400

    add IMovieRepository

---
+++ MovieApp/MovieApp.Core/Interfaces/IMovieRepository.cs
@@ -0,0 +1,12 @@
+﻿using MovieApp.Core.Entities;
+
+namespace MovieApp.Core.Interfaces;
+
+public interface IMovieRepository
+{
+   Task<Movie?> GetByIdAsync(int id);
+   Task<IList<Movie>> GetAllAsync();    
+   Task AddAsync(Movie movie);
+   Task UpdateAsync(Movie movie);
+   Task DeleteAsync(int Id);
+}
