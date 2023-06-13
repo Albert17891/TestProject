206852a55d1b08a9086d476c855fde1e0f2d4dac
Albert <abogevorgian@gmail.com>
2023-06-13 17:32:34 +0400

    IMovieService

---
+++ MovieApp/MovieApp.Core/Interfaces/IBaseRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace MovieApp.Core.Interfaces;
+
+public interface IBaseRepository<T>
+{
+    IQueryable<T> Table { get; }   
+    Task<IList<T>> GetAllAsync(CancellationToken token);
+    Task<bool> AddAsync(T movie, CancellationToken token);
+    Task<bool> UpdateAsync(T movie);
+    Task<bool> DeleteAsync(T entity, CancellationToken token);
+}
--- MovieApp/MovieApp.Core/Services/MovieService.cs
+++ MovieApp/MovieApp.Core/Services/MovieService.cs
@@ -1,4 +1,6 @@
-﻿using MovieApp.Core.Entities.MovieModels;
+﻿using Mapster;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.Core.Entities.MovieModels;
 using MovieApp.Core.Interfaces;
 using MovieApp.Core.Interfaces.Services;
 
@@ -6,34 +8,49 @@ namespace MovieApp.Core.Services;
 
 public class MovieService : IMovieService
 {
-    private readonly IMovieRepository _repository;
+    private readonly IBaseRepository<Movie> _repository;
 
-    public MovieService(IMovieRepository repository)
+    public MovieService(IBaseRepository<Movie> repository)
     {
-        _repository=repository;
+        _repository = repository;
     }
-    public Task AddMovieAsync(MovieRequest movie, CancellationToken token)
+    public async Task<bool> AddMovieAsync(MovieRequest movie, CancellationToken token)
     {
-        throw new NotImplementedException();
-    }
+        if (movie == null)
+            throw new ArgumentNullException(nameof(movie));
 
-    public Task DeleteMovieAsync(int Id, CancellationToken token)
-    {
-        throw new NotImplementedException();
+       return await _repository.AddAsync(movie.Adapt<Movie>(), token);
     }
 
-    public Task<IList<MovieServiceModel>> GetAllMovieAsync(CancellationToken token)
+    public async Task<bool> DeleteMovieAsync(int id, CancellationToken token)
     {
-        throw new NotImplementedException();
+        var movie = await _repository.Table.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (movie == null)
+            throw new NullReferenceException();
+
+       return await _repository.DeleteAsync(movie, token);
     }
 
-    public Task<MovieServiceModel?> GetMovieByIdAsync(int id, CancellationToken token)
+    public async Task<IList<MovieServiceModel>> GetAllMovieAsync(CancellationToken token)
     {
-        throw new NotImplementedException();
+        var movies = await _repository.GetAllAsync(token);
+
+        return movies.Adapt<IList<MovieServiceModel>>();
     }
 
-    public Task UpdateMovieAsync(MovieUpdateRequest movie)
+    public async Task<MovieServiceModel?> GetMovieByIdAsync(int id, CancellationToken token)
     {
-        throw new NotImplementedException();
+        var movie = await _repository.Table.FirstOrDefaultAsync(x => x.Id == id);
+
+        return movie?.Adapt<MovieServiceModel>();
+    }
+
+    public async Task<bool> UpdateMovieAsync(MovieUpdateRequest movie)
+    {
+        if (movie == null)
+            throw new NullReferenceException();
+
+        return await _repository.UpdateAsync(movie.Adapt<Movie>());
     }
 }
