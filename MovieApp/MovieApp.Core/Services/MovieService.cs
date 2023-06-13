585d59e456117238a872af41f5fd1f924e6b681f
Albert <abogevorgian@gmail.com>
2023-06-13 16:58:16 +0400

    add new service

---
+++ MovieApp/MovieApp.Core/Services/MovieService.cs
@@ -0,0 +1,39 @@
+﻿using MovieApp.Core.Entities.MovieModels;
+using MovieApp.Core.Interfaces;
+using MovieApp.Core.Interfaces.Services;
+
+namespace MovieApp.Core.Services;
+
+public class MovieService : IMovieService
+{
+    private readonly IMovieRepository _repository;
+
+    public MovieService(IMovieRepository repository)
+    {
+        _repository=repository;
+    }
+    public Task AddMovieAsync(MovieRequest movie, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteMovieAsync(int Id, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IList<MovieServiceModel>> GetAllMovieAsync(CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<MovieServiceModel?> GetMovieByIdAsync(int id, CancellationToken token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateMovieAsync(MovieUpdateRequest movie)
+    {
+        throw new NotImplementedException();
+    }
+}
